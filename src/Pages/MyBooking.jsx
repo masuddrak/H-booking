5d4422c3950617a97a2f0f9d10095a84daac38fb
masud <masudranaacb@gmail.com>
2024-05-11 01:38:08 +0600

    added my booking list room table rows show

--- src/Pages/MyBooking.jsx
+++ src/Pages/MyBooking.jsx
@@ -1,8 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 const MyBooking = () => {
+    const { user } = useAuth()
+    const [bookingRooms, setBookingRooms] = useState([])
+    useEffect(() => {
+        const handelBookData = async () => {
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/mybookedlist/${user?.email}`)
+            setBookingRooms(data)
+        }
+        handelBookData()
+    }, [user])
+    console.log(bookingRooms)
     return (
-        <div>
-            Thisis my booking page
+        <div className="container mx-auto">
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Room</th>
+                            <th>Room Size</th>
+                            <th>Price per Night</th>
+                            <th>Bookig Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            bookingRooms.map(room => <tr key={room._id}>
+                                <td>
+                                        <div className="avatar">
+                                            <div className="mask w-12 h-12">
+                                                <img src={room.bookImage} alt="bookImage" />
+                                            </div>
+                                        </div>
+                                </td>
+                                <td>
+                                   
+                                    <span >{room.bookSize}sqb</span>
+                                </td>
+                                <td>
+                                      
+                                <span >${room.bookPrice}</span>
+                                </td>
+                                <td>
+                                      
+                                <span >{room.startDate}</span>
+                                </td>
+                                <th className="flex gap-4">
+                                    <button className="bg-red-400 px-2 py-1 text-white rounded-sm">Cencel</button>
+                                    <button className="bg-green-600 px-2 py-1 text-white rounded-sm">Update</button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/components/RoomDetails.jsx
+++ src/components/RoomDetails.jsx
@@ -1,9 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+import { toast } from "react-toastify";
 const RoomDetails = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [showModal, setShowModal] = useState(false);
     const loaderData = useLoaderData()
-    console.log(loaderData.data)
+    const { user } = useAuth()
     const { _id, images3, images2, images1, Availability, Price, Size, Description, Offers } = loaderData?.data || {}
+
+
+
+
+    const heldelBook = (e) => {
+        e.preventDefault()
+        const from = e.target
+        const email = from.email.value
+        const bookId = _id
+        const bookImage = images1
+        const bookPrice = Price
+        const bookSize = Size
+        const Availability = "unavailable"
+        const bookInfo = { email, bookId, bookImage, bookPrice, bookSize, startDate }
+        try {
+            const { data } = axios.patch(`${import.meta.env.VITE_API_URL}/availability/${_id}`, { Availability })
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+        if (Availability==="unavailable") {
+            return toast.warning("it`s Already Booked")
+        }
+        try {
+            const { data } = axios.post(`${import.meta.env.VITE_API_URL}/bookrooms`, bookInfo)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     return (
         <div className="container m-auto">
             <div className="grid grid-cols-3">
@@ -22,7 +62,82 @@ const RoomDetails = () => {
                     <p><span className="font-bold text-base-content">Room Size: </span>{Size}</p>
                     <p><span className="font-bold text-base-content">Availabilit: </span>{Availability}</p>
                     <p><span className="font-bold text-base-content">Special Offers: </span>{Offers}</p>
-                    <button className="bg-base-content px-2 py-1 text-white rounded-sm">Book Now</button>
+                    {/*  */}
+                    <div>
+                        <button
+                            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            type="button"
+                            onClick={() => setShowModal(true)}
+                        >
+                            Book Now
+                        </button>
+                        {showModal ? (
+                            <>
+                                <div
+                                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none  "
+                                >
+                                    <div className="relative w-auto my-6 mx-auto  max-w-3xl">
+                                        {/*content*/}
+                                        <div className="border-0 rounded-lg shadow-lg px-3 py-5  relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                            {/*header*/}
+                                            <div className="flex items-start justify-between rounded-t">
+                                                <h3 className="text-3xl font-semibold">
+                                                    Room ID: {_id}
+                                                </h3>
+                                                <button
+                                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                    onClick={() => setShowModal(false)}
+                                                >
+                                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                        ×
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            {/*body*/}
+                                            <div className="relative p-6 flex-auto">
+                                                <form onSubmit={heldelBook} className="space-y-3">
+                                                    <div>
+                                                        <p><span className="font-bold text-base-content">Price per Night: </span>${Price}</p>
+                                                        <p><span className="font-bold text-base-content">Room Size: </span>{Size}</p>
+                                                        <p><span className="font-bold text-base-content">Availabilit: </span>{Availability}</p>
+                                                        <p><span className="font-bold text-base-content">Special Offers: </span>{Offers}</p>
+                                                    </div>
+                                                    <div className="space-y-1 text-sm">
+                                                        <label htmlFor="Name" className="block text-white"> User Email</label>
+                                                        <input type="email" name="email" defaultValue={user?.email} readOnly placeholder="User Email" className="w-full px-2 outline-0 py-4 border-b-[1px] border-gray-400" />
+                                                    </div>
+                                                    <div className="w-full">
+                                                        <label htmlFor="Name" className="block text-base-content"> Select Date</label>
+                                                        <DatePicker className="w-full px-2 outline-0 py-4 border-b-[1px] border-gray-400 mb-4" selected={startDate} onChange={(date) => setStartDate(date)} />
+                                                    </div>
+                                                    <input
+                                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 absolute right-0"
+                                                        type="submit"
+                                                        // onClick={() => setShowModal(false)}
+                                                        value="Book"
+                                                    />
+
+                                                </form>
+                                            </div>
+                                            {/*footer*/}
+                                            <div className="flex items-center justify-end  rounded-b pb-6">
+                                                <button
+                                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 absolute left-0"
+                                                    type="button"
+                                                    onClick={() => setShowModal(false)}
+                                                >
+                                                    Close
+                                                </button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                            </>
+                        ) : null}
+                    </div>
+                    {/*  */}
                 </div>
             </div>
         </div>
